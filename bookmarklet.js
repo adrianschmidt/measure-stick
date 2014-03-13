05a485d5f3fd33351fbdab25d9fb38c83c820c01
Adrian Schmidt <adrian@stealmycode.se>
2014-03-13 13:07:47 +0100

    Adds label with current size
    Credits to Linus Karnland for this addition.

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,7 +1,7 @@
-javascript:(function($) {
-
+javascript: (function($) {
 	var jqui = $.getScript('//code.jquery.com/ui/1.10.3/jquery-ui.js', function() {
 		var ms = $('<div></div>');
+		var label = $('<span></span>');
 		ms.css({
 			backgroundColor: 'orange',
 			height: '10px',
@@ -10,11 +10,24 @@ javascript:(function($) {
 			top: '20px',
 			width: '10px',
 			zIndex: '999999'
-		}).appendTo('body').draggable({ grid: [ 20, 20 ] });
-
+		}).appendTo('body');
+		ms.draggable({
+			grid: [20, 20],
+			drag: function() {
+				updateLabel()
+			}
+		});
+		label.css({
+			position: 'fixed',
+			zIndex: '999999',
+			backgroundColor: '#000000',
+			color: '#FFFFFF',
+			top: '0',
+			right: '0',
+			padding: '5px',
+		}).appendTo('body');
 		$(document).keydown(function(e) {
 			var step = e.shiftKey ? 10 : 1;
-
 			switch (e.keyCode) {
 			case 37: /* left */
 				if (e.altKey) {
@@ -44,12 +57,31 @@ javascript:(function($) {
 					ms.css('top', (parseInt(ms.offset().top, 10) + step) + 'px');
 				}
 				break;
-			default:
-				return true;
 			}
-
+			updateLabel();
 			return false;
 		});
+		var updateLabel = function() {
+			label.html(ms.width() + ', ' + ms.height());
+			var msBorderTop = parseInt(ms.offset().top, 10);
+			var msBorderRight = parseInt(ms.offset().left, 10) + ms.width();
+			var labelBorderTop = label.height() + 20;
+			var labelBorderLeft = parseInt(label.offset().left, 10) - 20;
+			var labelAtTop = parseInt(label.offset().top, 10) < 1;
+			if (msBorderTop < labelBorderTop && msBorderRight > labelBorderLeft) {
+				label.css({
+					right: 0,
+					top: '',
+					bottom: 0
+				});
+			} else {
+				label.css({
+					right: 0,
+					top: 0,
+					bottom: ''
+				});
+			}
+		};
+		updateLabel();
 	});
-
 }(jQuery))
