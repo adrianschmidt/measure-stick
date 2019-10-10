ed031369b8204eced70bfd37cd47bf57a588e6b6
Adrian Schmidt <adrian.schmidt@lime.tech>
2019-10-10 10:53:41 +0200

    fix: use position:fixed instead of position:absolute

--- bookmarklet.js
+++ bookmarklet.js
@@ -23,7 +23,7 @@ javascript:
 				backgroundColor: 'orange',
 				height: '10px',
 				left: '20px',
-				position: 'absolute',
+				position: 'fixed',
 				top: '20px',
 				width: '10px',
 				zIndex: '999999'
@@ -50,28 +50,28 @@ javascript:
 					if (e.altKey) {
 						ms.width(ms.width() - step);
 					} else {
-						ms.css('left', (parseInt(ms.offset().left, 10) - step) + 'px');
+						ms.css('left', (parseInt(ms.position().left, 10) - step) + 'px');
 					}
 					break;
 				case 38: /* up */
 					if (e.altKey) {
 						ms.height(ms.height() - step);
 					} else {
-						ms.css('top', (parseInt(ms.offset().top, 10) - step) + 'px');
+						ms.css('top', (parseInt(ms.position().top, 10) - step) + 'px');
 					}
 					break;
 				case 39: /* right */
 					if (e.altKey) {
 						ms.width(ms.width() + step);
 					} else {
-						ms.css('left', (parseInt(ms.offset().left, 10) + step) + 'px');
+						ms.css('left', (parseInt(ms.position().left, 10) + step) + 'px');
 					}
 					break;
 				case 40: /* down */
 					if (e.altKey) {
 						ms.height(ms.height() + step);
 					} else {
-						ms.css('top', (parseInt(ms.offset().top, 10) + step) + 'px');
+						ms.css('top', (parseInt(ms.position().top, 10) + step) + 'px');
 					}
 					break;
 				default:
